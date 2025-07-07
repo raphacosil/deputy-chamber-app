7b885bcfe6367cd7f0d179485ee00b9c6e63d7a2
RaphaCosil <Raphael.Cosil@gmail.com>
2025-07-07 13:31:34 -0300

    Deputy screens

---
+++ app/src/main/java/com/example/deputy_chamber_app/DeputyActivity.kt
@@ -0,0 +1,11 @@
+package com.example.deputy_chamber_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DeputyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deputy)
+    }
+}
